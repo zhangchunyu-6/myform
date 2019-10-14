f24f5e3d22b76c299f02291cfb0368fe3d753684
zhangchunyu-6 <1102582521@qq.com>
2019-10-14 22:29:21 +0800

    wechar

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -8,7 +8,7 @@ class EventController extends Controller
 {
     public function event(Tools $tools)
     {
-        echo storage_path();
+      
         $info= file_get_contents("php://input");
          file_put_contents(storage_path('logs/wecahr'.date('Y-m-d').'.log'),$info,FILE_APPEND);
 
