49c197e7c9f72b3d5ad329a6c1d378ff7c0ddd2a
zhangchunyu-6 <1102582521@qq.com>
2019-10-14 23:54:43 +0800

    m-mmwechar

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -10,10 +10,14 @@ class EventController extends Controller
     {
       
         $info= file_get_contents("php://input");
-        file_put_contents(storage_path('logs/wechar/'.date('Y-m-d').'.log'),">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n",$info,FILE_APPEND);
-         file_put_contents(storage_path('logs/wechar/'.date('Y-m-d').'.log'),$info,FILE_APPEND);
-        $xml_obj=simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);
-        $xml_arr=(array)$xml_obj;
+        file_put_contents(storage_path('logs/wechar/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
+
+        file_put_contents(storage_path('logs/wechar/'.date('Y-m-d').'.log'),$info,FILE_APPEND);
+
+        $xml_obj = simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);
+
+        $xml_arr = (array)$xml_obj;
+
         dd($xml_arr);
     }                                                                                                                                    
 }
