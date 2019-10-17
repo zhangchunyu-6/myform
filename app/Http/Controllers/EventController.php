40a954d251bff1501c5029293cac4b5af718a58a
zhangchunyu-6 <1102582521@qq.com>
2019-10-17 23:28:17 +0800

    aa

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -9,7 +9,9 @@ class EventController extends Controller
     public function event(Tools $tools)
     {
       
+
         $info= file_get_contents("php://input");
+        dd($info);
         file_put_contents(storage_path('logs/wechar/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
 
         file_put_contents(storage_path('logs/wechar/'.date('Y-m-d').'.log'),$info,FILE_APPEND);
@@ -33,18 +35,49 @@ class EventController extends Controller
             //普通的信息发送
             if($xml_arr['MsgType']=='text'){
               
-                $media_id="K3BH1WlREhi3cKRqx2gI9i1q-dZqCezx35wed4T2j2tWxGjLQf_w_-wqEZz9A8LD";
-                echo"<xml>
-                <ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName>
+                $media_id="您好 新的一天又要开始了 你努力了吗？";
+                echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName>
                 <FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName>
                 <CreateTime>".time()."</CreateTime>
                 <MsgType><![CDATA[voice]]></MsgType>
-                <Voice>
-                <MediaId><![CDATA[".$media_id."]]></MediaId>
-                </Voice>
+                <Voice><MediaId><![CDATA[".$media_id."]]></MediaId></Voice>
                 </xml>";
               }
-          
+              
+  
+
+
+              $url="https://api.weixin.qq.com/cgi-bin/menu/create?access_token=".$tools->get_access_token();
+              $data=[
+                  "button"=>[
+                      [	
+                           "type"=>"click",
+                           "name"=>"签到",
+                           "key"=>"V1001_TODAY_MUSIC"
+                      ],
+                          [
+                            "name"=>"菜单",
+                            "sub_button"=>[
+                            [	
+                                "type"=>"view",
+                                "name"=>"搜索",
+                                "url"=>"http://www.soso.com/"
+                            ],
+                             [
+                                "type"=>"click",
+                                "name"=>"赞一下我们",
+                                "key"=>"V1001_GOOD"
+                             ]
+                          ]
+                        ]
+                    ]
+              ];
+              dd($data);
+              $re=$tools->curl_post($url,json_encode($data,JSON_UNESCAPED_UNICODE));
+              $result=\json_decode($re,true);
+              echo createMenu($result);
+
+              
           }   
                                                                                                                                        
 }
--- app/Http/Controllers/WecharController.php
+++ app/Http/Controllers/WecharController.php
@@ -22,38 +22,5 @@ class WecharController extends Controller
        //dd($result);
         return view('wechar.user_list',['list'=>$result['data']['openid'],'tag_id'=>$request->input('tag_id')]);
     }
-
-    public function button(Tools $tools)
-    {
-        $url="https://api.weixin.qq.com/cgi-bin/menu/create?access_token=".$tools->get_access_token();
-        $data=[
-            "button"=>[
-                [	
-                     "type"=>"click",
-                     "name"=>"今日歌曲",
-                     "key"=>"V1001_TODAY_MUSIC"
-                ],
-                    [
-                      "name"=>"菜单",
-                      "sub_button"=>[
-                      [	
-                          "type"=>"view",
-                          "name"=>"搜索",
-                          "url"=>"http://www.soso.com/"
-                      ],
-                       [
-                          "type"=>"click",
-                          "name"=>"赞一下我们",
-                          "key"=>"V1001_GOOD"
-                       ]
-                    ]
-                  ]
-              ]
-        ]; 
-        $re=$tools->curl_post($url,json_encode($data,JSON_UNESCAPED_UNICODE));
-        $result=\json_decode($re,true);
-        echo createMenu($result);
-        
-    }
 }
 
