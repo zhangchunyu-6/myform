ea4b812a07f238a28d7ca9c947a53223a3dffae7
zhangchunyu-6 <1102582521@qq.com>
2019-10-23 19:11:23 +0800

    2019-10-23

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Tools\Tools;
+use App\Model\Openid;
 class EventController extends Controller
 {
+
     public function event(Tools $tools)
     {
+
         $info= file_get_contents("php://input");
 
         file_put_contents(storage_path('logs/wechar/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
@@ -16,31 +19,53 @@ class EventController extends Controller
 
         $xml_obj = simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);
 
-        $xml_arr = (array)$xml_obj;
+        $xml_arr =(array)$xml_obj;
 
-           if($xml_arr['MsgType'] == 'event' && $xml_arr['Event'] == 'subscribe'){
-               $wechar_user = $tools->get_wechar_user($xml_arr['FromUserName']);
-               $msg = '欢迎'.$wechar_user['nickname'].'同学'.'，感谢您的关注';
-                     echo "<xml>
-                           <ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName>
-                          <FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName>
-                          <CreateTime>".time()."</CreateTime>
-                          <MsgType><![CDATA[text]]></MsgType>
-                          <Content><![CDATA[".$msg."]]></Content>
-                          </xml>";
-            }
-            //普通的信息发送
-            if($xml_arr['MsgType']=='text' && $xml_arr['Content'] == '111'){
+               if($xml_arr['MsgType'] == 'event' && $xml_arr['Event'] == 'subscribe'){
+                    //判断open表当前是否有openid
+                    $openid_info= Openid::where(['openid'=>$xml_arr['FromUserName']])->first();
+                    if(empty($openid_info)){
+                        //首次关注
+                        if(isset($xml_arr['Ticket'])){
+                            //带参数
+                            $share_code= explode("_",$xml_arr['EventKey'])[1];
+                            Openid::insert([
+                                'uid'=>$share_code,
+                                'openid'=>$xml_arr['FromUserName'],
+                                'subscribe'=>1
+                            ]);
+                            User::where(['id'=>$share_code])->increment('share_num',1);//加业绩
 
-                $media_id="您好 新的一天又要开始了 你努力了吗？";
-                echo "<xml>
-                           <ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName>
-                          <FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName>
-                          <CreateTime>".time()."</CreateTime>
-                          <MsgType><![CDATA[text]]></MsgType>
-                          <Content><![CDATA[".$media_id."]]></Content>
-                          </xml>";
-              }
+                        }else{
+                            //普通关注；
+                            Openid::insert([
+                                'uid'=>0,
+                                'openid'=>$xml_arr['FromUserName'],
+                                'subscribe'=>1
+                            ]);
+                        }
+                    }
+
+                   $wechar_user = $tools->get_wechar_user($xml_arr['FromUserName']);
+                   $msg = '欢迎'.$wechar_user['nickname'].'同学'.'，感谢您的关注';
+                         echo "<xml>
+                               <ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName>
+                              <FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName>
+                              <CreateTime>".time()."</CreateTime>
+                              <MsgType><![CDATA[text]]></MsgType>
+                              <Content><![CDATA[".$msg."]]></Content>
+                              </xml>";
+                      }
+                    //普通的信息发送
+                    if($xml_arr['MsgType']=='text' && $xml_arr['Content'] == '111'){
+                        $media_id="您好 新的一天又要开始了 你努力了吗？";
+                        echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName>
+                        <FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName>
+                        <CreateTime>".time()."</CreateTime>
+                        <MsgType><![CDATA[text]]></MsgType>
+                        <Content><![CDATA[".$media_id."]]></Content>
+                        </xml>";
+                      }
 
      }
 
---
+++ app/Model/Openid.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Openid extends Model
+{
+    protected $table="openid";
+    protected $primaryKey="id";
+    public $timestamps ="false";
+}
